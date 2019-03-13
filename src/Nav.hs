bd7456bd537de30a8e4f8f4a70ce62b444a6f227
Mark Smith <smith.h.mark@gmail.com>
2019-03-13 15:03:22 -0700

    adding ability to parse location codes from HTML

--- src/Nav.hs
+++ src/Nav.hs
@@ -2,12 +2,36 @@ module Nav where
 --module Abbreviations where
 
 import qualified Data.Map as Map
+import Text.XML.HXT.Core
+import Text.HandsomeSoup
+import Data.Char (isSpace)
 
 twoLetterCodes = [
         ("Afghanistan", "AF"),
         ("Albania", "AL")
         ]
 
+type CountryName = String
+type CC2 = String
+type LocationCode = String
+
+cc2Table :: Map.Map CountryName CC2
 cc2Table = Map.fromList twoLetterCodes
 
+findCC2 :: CountryName -> Maybe CC2
 findCC2 cname = Map.lookup cname cc2Table
+
+
+cleanCountryName :: CountryName -> CountryName
+cleanCountryName = dropWhile isSpace . reverse . dropWhile isSpace . reverse
+
+cleanCCMapping :: [(LocationCode, CountryName)] -> [(CountryName, LocationCode)]
+cleanCCMapping = map (\(c, n) -> (cleanCountryName n, c)) . 
+  filter (\(c, n) -> c /= "")
+
+parseCCMapping :: String -> IO [(CountryName, LocationCode)]
+parseCCMapping html = do
+        let doc = parseHtml html
+        codes <- runX $ doc >>> css "#search-place option" >>> (getAttrValue "data-place-code" &&& (deep getText))
+        return $ cleanCCMapping codes
+
--- test/NavSpec.hs
+++ test/NavSpec.hs
@@ -1,16 +1,43 @@
 module NavSpec where
 
 import Nav
+import System.IO.Unsafe
 
 import Test.Hspec (Spec, describe, context, it, shouldBe)
 
+sampleHtml = "<html>\n\
+        \<head></head>\n\
+        \<body>\n\
+        \<div>\n\
+        \<form action=\"#\" method=\"GET\"> \n\
+        \<select name=\"place_selector\" class=\"place_selector\" id='search-place'\n\
+        \ onchange=\"document.location.href= this.value;\" \n\
+        \ aria-label='Use this to select the country page to view'> \n\
+        \ <option value=\"\">Please select a country to view</option> \n\
+        \ <option value=\"../geos/xx.html\" data-place-code=\"xx\"> \n\
+        \  World \n\
+        \ </option> \n\
+        \ <option value=\"../geos/af.html\" data-place-code=\"af\">\n Afghanistan </option> \n\
+        \<option value=\"../geos/ax.html\" data-place-code=\"ax\"> Akrotiri \t\n</option>\n\
+        \ </select>\n\
+        \</form> </div> </body>\n</html>"
+
+expectedMapping = [ 
+  ("World", "xx"),
+  ("Afghanistan", "af"),
+  ("Akrotiri", "ax")
+  ]
+
 
 spec :: Spec
-spec =
-        describe "Look up 2-letter codes" $ do 
-                it "Shows Afghanistan as AF" $ do
-                        Nav.findCC2 "Afghanistan" `shouldBe` Just "AF"
-                it "Shows Albania as AL" $ do
-                        Nav.findCC2 "Albania" `shouldBe` Just "AL"
-                it "Shows Unitied States as US" $ do
-                        Nav.findCC2 "United States" `shouldBe` Just "US"
+spec = do
+  describe "Look up 2-letter codes" $ do
+    it "Shows Afghanistan as AF" $ do
+      Nav.findCC2 "Afghanistan" `shouldBe` Just "AF"
+    it "Shows Albania as AL" $ do
+      Nav.findCC2 "Albania" `shouldBe` Just "AL"
+    it "Shows Unitied States as US" $ do
+      Nav.findCC2 "United States" `shouldBe` Nothing
+  describe "build cc2 mapping" $ do 
+    it "parses html for the options box" $ do
+      unsafePerformIO (Nav.parseCCMapping sampleHtml) `shouldBe` expectedMapping
